a49a873bd5385d8d0c3955e8d83911b64ae84ce9
Eric Svebakk <ericatsvebakk@gmail.com>
2025-09-10 09:55:57 +0000

    Refactord gene panel

--- frontend/lib/components/navigation/NavbarRight.tsx
+++ frontend/lib/components/navigation/NavbarRight.tsx
@@ -1,32 +1,16 @@
 
-import { Autocomplete, Box, Button, Collapse, createFilterOptions, Grid, Link, Stack, SxProps, TextField, Typography } from "@mui/material";
-import React, { useEffect, useRef, useState } from "react";
+import { Box, Button, Grid, Stack, SxProps, Typography } from "@mui/material";
+import React, { useState } from "react";
 import { theme } from "@/app/layout";
-import { GroupWork } from "@mui/icons-material";
-import { FeatureScatterPlot } from "../plots/FeatureScatterPlot";
-import { useAppDispatch, useAppSelector } from "@/lib/redux/hooks/hooks";
-import { AutocompleteOption } from "@/lib/types";
-import { get_feature_coordinates } from "@/lib/fetch/get_feature_coordinates";
-import { LoadingButton } from "@mui/lab";
-import get_ncbi_gene_summary from "@/lib/fetch/get_ncbi_gene_summary";
-
+import { FileOpen, GroupWork } from "@mui/icons-material";
+import PanelGenes from "./panel/PanelGenes";
+import PanelFiles from "./panel/PanelFiles";
 
 export default function NavbarRight() {
   
-  const fileID = useAppSelector((state) => state.fileReducer.activeFile);  
-  const genes = useAppSelector((state) => state.plotReducer.data.genes);
-  const status = useAppSelector((state) => state.plotReducer.status.get_feature_coordinates);
-  const report = useAppSelector((state) => state.plotReducer.data.geneReport);
-  
-  const ref = useRef();
-  const dispatch = useAppDispatch();  
 
-  const [open, setOpen] = useState(false);
-  const [selectedGene, setSelectedGene] = useState<AutocompleteOption | null>(null);  
-  const [geneOptionsFiltered, setGeneOptionsFiltered] = useState<AutocompleteOption[]>([]);
-    
-  const unsFilterOptions = createFilterOptions({ limit: 20 });
-  
+  const [openPanelGenes, setOpenPanelGenes] = useState(false);
+  const [openPanelFiles, setOpenPanelFiles] = useState(false);
   
   const navItemProps = (isOpen: boolean, index: number | null = null) => {
     return {
@@ -46,232 +30,10 @@ export default function NavbarRight() {
     } as SxProps;
   };
   
-  
-    useEffect(() => {
-      if (genes) {
-        const genesStructuredOptions: AutocompleteOption[] = genes.map(
-          (e, i) => ({ label: e, id: i })
-        );
-  
-        setGeneOptionsFiltered(genesStructuredOptions);
-      }
-    }, [genes]);
-  
   return (
     <Box height="100%">
-      <Collapse
-        unmountOnExit
-        ref={ref}
-        in={open}
-        orientation="horizontal"
-        sx={{
-          position: "absolute",
-          right: 85,
-          top: 0,
-          height: "100%",
-          zIndex: 500,
-        }}
-      >
-        <Grid
-          container
-          direction="column"
-          height="100%"
-          width={300}
-          rowGap={1}
-          sx={{
-            p: 1,
-            borderLeft: "1px solid grey",
-            backgroundColor: theme.palette.primary.main,
-            WebkitBoxShadow: "-1px 0 2px -1px #000000",
-            boxShadow: "-1px 0 2px -1px #000000",
-          }}
-        >
-          <Grid item>
-            <FeatureScatterPlot />
-          </Grid>
-
-          <Grid item>
-            <Stack
-              direction="column"
-              rowGap={1}
-              sx={{
-                border: "1px solid grey",
-                p: 1,
-                pt: 2,
-                backgroundColor: theme.palette.background.paper,
-              }}
-            >
-              <Autocomplete
-                disabled={genes.length === 0}
-                size="small"
-                fullWidth
-                value={selectedGene}
-                options={geneOptionsFiltered}
-                isOptionEqualToValue={(option, value) =>
-                  (option as AutocompleteOption).id ===
-                  (value as AutocompleteOption).id
-                }
-                onChange={(event: any, value: any, reason, details) => {
-                  const selectedOption = details?.option as any;
-
-                  if (reason === "selectOption") {
-                    setSelectedGene(selectedOption);
-                  } else if (reason === "removeOption") {
-                    setSelectedGene(null);
-                  } else if (reason === "clear") {
-                    setSelectedGene(null);
-                  }
-                }}
-                filterOptions={unsFilterOptions}
-                noOptionsText="No matching gene"
-                renderInput={(params) => {
-                  return (
-                    <TextField
-                      {...params}
-                      label="Select gene"
-                      placeholder="gene"
-                      InputLabelProps={{ shrink: true }}
-                    />
-                  );
-                }}
-              />
-
-              <LoadingButton
-                variant="outlined"
-                size="medium"
-                loading={status.inProgress}
-                onClick={() => {
-                  if (selectedGene) {
-                    get_feature_coordinates(
-                      fileID as string,
-                      selectedGene.label,
-                      dispatch
-                    );
-
-                    get_ncbi_gene_summary(selectedGene.label, dispatch);
-                  }
-                }}
-              >
-                Fetch feature data
-              </LoadingButton>
-            </Stack>
-          </Grid>
-
-          <Grid
-            item
-            xs
-            sx={{
-              p: 1,
-              height: "100%",
-              backgroundColor: theme.palette.background.paper,
-              border: "1px solid grey",
-            }}
-          >
-            <Stack
-              direction="column"
-              sx={{
-                height: "100%",
-              }}
-            >
-              <Stack
-                direction="row"
-                sx={{
-                  width: "100%",
-                  justifyContent: "end",
-                }}
-              >
-                <Typography
-                  variant="caption"
-                >
-                  Gene Summary
-                </Typography>
-              </Stack>
-              
-              {report ? (
-                <Stack
-                  direction="column"
-                  sx={{
-                    height: "100%",
-                  }}
-                >
-                  <Stack
-                    direction="column"
-                    rowGap={1}
-                    sx={{
-                      height: "100%",
-                    }}
-                  >
-                    <Typography
-                      sx={{
-                        fontWeight: "bold",
-                      }}
-                    >
-                      {report.symbol}
-                    </Typography>
-
-                    <Typography
-                      sx={{
-                        fontStyle: "italic",
-                      }}
-                      variant="body2"
-                    >
-                      {report.description}
-                    </Typography>
-
-                    {report.summary.map((e, i) => {
-                      return (
-                        <Typography key={"report_summary_" + i} variant="body2">
-                          {e}
-                        </Typography>
-                      );
-                    })}
-                    <Stack direction="column" rowGap={0}>
-                      <Typography variant="body2">Synonyms</Typography>
-                      <Typography variant="body2">
-                        [{report.synonyms.join(", ")}]
-                      </Typography>
-                    </Stack>
-                  </Stack>
-                  
-                  <Stack
-                    direction="row"
-                    sx={{
-                      width: "100%",
-                      justifyContent: "end",
-                    }}
-                  >
-                    <Link
-                      href={report.source}
-                      underline="hover"
-                      variant="body2"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      [Source]
-                    </Link>
-                  </Stack>
-                </Stack>
-              ) : (
-                <Box
-                  sx={{
-                    height: "100%",
-                    width: "100%",
-                    alignContent: "center",
-                    justifyItems: "center"
-                  }}
-                >
-                  <Typography variant="body2">
-                    No gene selected
-                  </Typography>
-                </Box>
-              )}
-
-
-            </Stack>
-          </Grid>
-
-        </Grid>
-      </Collapse>
+      <PanelGenes open={openPanelGenes} setOpen={setOpenPanelGenes}/>
+      {/* <PanelFiles open={openPanelFiles} setOpen={setOpenPanelFiles}/> */}
 
       <Grid
         item
@@ -283,12 +45,35 @@ export default function NavbarRight() {
           zIndex: 1200,
         }}
       >
+        {/* <Grid item height={70}>
+          <Button
+            fullWidth
+            sx={navItemProps(openPanelFiles)}
+            onClick={() => {
+              setOpenPanelFiles(!openPanelFiles);
+            }}
+          >
+            <Stack
+              direction="column"
+              sx={{
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              {<FileOpen />}
+              <Typography fontSize={theme.typography.subtitle1.fontSize}>
+                Select file
+              </Typography>
+            </Stack>
+          </Button>
+        </Grid> */}
+        
         <Grid item height={70}>
           <Button
             fullWidth
-            sx={navItemProps(open)}
+            sx={navItemProps(openPanelGenes)}
             onClick={() => {
-              setOpen(!open);
+              setOpenPanelGenes(!openPanelGenes);
             }}
           >
             <Stack
