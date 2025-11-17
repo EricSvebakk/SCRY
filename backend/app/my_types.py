713c179460d58f3662880121442f08722887c904
Eric Svebakk <ericatsvebakk@gmail.com>
2025-11-17 10:15:20 +0000

    Added forgotten changes

--- backend/app/my_types.py
+++ backend/app/my_types.py
@@ -15,7 +15,9 @@ class newObservation(BaseModel):
 class ValidationResponse(TypedDict):
   response: str
   ok: bool
+  code: str
   
 class ComputationResponse(TypedDict):
   response: Any
   ok: bool
+  code: str
